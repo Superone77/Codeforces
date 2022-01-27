1211ed9feb0e89e3f86298014a952af601126d99
One7 <yangwq177@163.com>
2022-01-27 18:50:14 +0100

    #768

--- #768/problem_c.cpp
+++ #768/problem_c.cpp
@@ -1,65 +1,45 @@
 //
 // Created by Superone77 on 2022/1/27.
 //
+
+// https://zhuanlan.zhihu.com/p/462561849
 #include <iostream>
 #include <vector>
 
 
 using namespace std;
 
-vector<pair<int,int>> compute(int &size, int &target, vector<int> &flags, int count){
-    if(count == 1){
-        for(int i = 0;i<size;i++){
-            if(flags[i]==1){
-                continue;
-            }
-            for(int j = i+1; j<size;j++){
-                if(flags[j] == 1) {
-                    continue;
-                }
-                if(i&j == target){
-                    pair<int,int> temp = {i,j};
-                    vector<pair<int,int>> res;
-                    res.push_back(temp);
-                    return res;
-                }
-            }
-        }
-        return NULL;
-    }
-    for(int i = 0;i<size;i++){
-        if(flags[i]==1){
-            continue;
-        }
-        flags[i] = 1;
-        for(int j = i+1; j<size;j++){
-            if(flags[j] == 1){
-                continue;
-            }
-            flags[j]= 1;
-            int temp = i&j;
-            vector<pair<int,int>> res = compute(size,target-temp flags, count-1);
-            flags[j]=0;
-
-        }
-        flags[i]=0;
-    }
-}
-
-
 int main() {
     int repeats = 0;
     cin>>repeats;
     for(int i = 0;i<repeats;++i){
-        int size;
-        int target;
-        cin>>size;
-        cin>>target;
-        vector<int> flags;
-        for(int i = 0;i<size;i++){
-            flags.push_back(0);
-        }
-        vector<pair<int,int>> res = compute(size,target, flags, size/2);
+        solve();
     }
+}
+
+void solve(){
+    int n,k;cin>>n>>k;
+    if(n == 4 && k==3){cout<<-1<<'n';return;}
+
+    vector<pair<int,int>> ans;
+    if(k==n-1){
+        ans.push_back(make_pair(n-1,n-2));
+        ans.push_back(make_pair(0,n-3));
+        ans.push_back(make_pair(1,3));
+        ans.push_back(make_pair(2.n-4));
+        for(int i =4;i<n/2;i++) ans.push_back(make_pair(i,n-1-i));
+    }
+    else if(k==0)
+    {
+        for(int i=0;i<n/2;i++) ans.push_back(make_pair(i,n-1-i));
+    }
+    else
+    {
+        ans.push_back(make_pair(n-1,k));
+        ans.push_back(make_pair(0,n-1-k));
+        for(int i = 1;i<n/2;i++) if(i!=k && n-1-i!=k) ans.push_back(make_pair(i,n-i-1));
+    }
+
+    for(auto [x,y]:ans) cout<<x<<" "<<y<<'\n';
 
 }
